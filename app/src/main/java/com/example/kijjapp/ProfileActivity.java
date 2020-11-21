b2c9ec32aad2aabaa0e308c431169ec87ff6685e
kaytinmatrangola <klmatrangola@loyola.edu>
2020-11-21 17:16:22 -0500

    changing to homepage.php in profile activity

--- app/src/main/java/com/example/kijjapp/ProfileActivity.java
+++ app/src/main/java/com/example/kijjapp/ProfileActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
-    public static final String URL = "http://kijj.cs.loyola.edu/model/testHi.php";
+    public static final String URL = "http://kijj.cs.loyola.edu/model/homepage.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

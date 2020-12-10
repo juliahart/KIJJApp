f13b35bbc660e81a13fcd1c432b7b44a8262d2da
kaytinmatrangola <klmatrangola@loyola.edu>
2020-12-10 12:54:16 -0500

    applied thing is iffy

--- app/src/main/java/com/example/kijjapp/NewBookingsActivity.java
+++ app/src/main/java/com/example/kijjapp/NewBookingsActivity.java
@@ -361,8 +361,13 @@ public class NewBookingsActivity extends AppCompatActivity {
         int bookingId = validBookingsList.get(position).getId();
         ThreadTaskDoApply taskApply = new ThreadTaskDoApply(this, bookingId);
         taskApply.start();
-        Button applyB = (Button) findViewById(R.id.apply);
-        applyB.setText("Applied");
+        //Button applyB = (Button) listView.getItemAtPosition(position) findView;
+        //Button applyB = (Button) findViewById(R.id.apply).;
+       // applyB.setText("Applied");
+
+
+        //validBookingsList.remove(position);
+        //listView.add(position, "followed");
 
     }
 }
